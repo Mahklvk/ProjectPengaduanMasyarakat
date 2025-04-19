84559d0dfd4314e69b4d35829644e814b0364dad
Mahklvk <nyehggg@gmail.com>
2025-04-19 07:42:05 +0700

    modified index

--- index.php
+++ index.php
@@ -7,9 +7,18 @@ require('config/db.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fontawesome/css/all.min.css">
     <title>Document</title>
 </head>
 <body>
+    <?php include('config/navbar.php')?>
     <h1>Hello world</h1>
+
+
+
+    <script src="assets/bootstrap/js/bootstrap.bundle.js"></script>
+    <script src="assets/fontawesome/js/all.min.js"></script>
+    <script src="https://unpkg.com/feather-icons"></script>
 </body>
 </html>
