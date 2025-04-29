19dd46322218107bf38f7c8253ce78303b4f23a5
Mahklvk <nyehggg@gmail.com>
2025-04-29 09:09:10 +0700

    add admin pages

---
+++ admin/listAccount.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container text-center">
+  <div class="row align-items-start">
+    <div class="col">
+      One of three columns
+    </div>
+    <div class="col">
+      One of three columns
+    </div>
+    <div class="col">
+      One of three columns
+    </div>
+  </div>
+</div>
+
+<script src="../assets/bootstrap/js/bootstrap.bundle.js"></script>
+</body>
+</html>
