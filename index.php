f68d9c6a33e1d4c55b0b1b25a6b440365255288c
Mahklvk <nyehggg@gmail.com>
2025-05-10 21:37:24 +0700

    added function for contact

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ require('config/db.php'); //include database
   <link rel="stylesheet" href="assets/fontawesome/css/all.min.css"> <!-- link fontawesome untuk bisa mengakses icon -->
   <link rel="stylesheet" href="assets/css/style.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <title>Document</title>
 </head>
 
@@ -44,7 +45,7 @@ require('config/db.php'); //include database
         <div class="container col-md-6 text text-justify mt-md-5">
           <p class="text">Sampaikan aspirasi dan keluhan Anda secara mudah melalui satu platform digital. Aplikasi Pelaporan Masyarakat hadir untuk menjembatani komunikasi antara masyarakat dan instansi, dengan proses yang cepat, transparan, dan responsif.</p>
           <div class="col-md-6 container align-items-center justify-content-center d-flex mb-5 mt-md-5">
-            <button class="btn btn-costum">Mulai Lapor</button>
+           <a href="tulisLaporan.php"> <button class="btn btn-costum">Mulai Lapor</button></a>
           </div>
         </div>
         <div class="container col-md-3 img-about">
@@ -66,17 +67,52 @@ require('config/db.php'); //include database
         <div class="col-md-7">
           <form action="" method="post">
             <i class="fa-solid fa-user"></i>
-            <input type="username" placeholder="Nama Kamu" class="form-input">
+            <input type="username" placeholder="Nama Kamu" class="form-input" name="nama">
             <br>
             <i class="fa-solid fa-envelope"></i>
-            <input type="email" placeholder="Email Kamu" class="form-input"><br>
-            <textarea name="" id="" placeholder="bagikan pendapatmu" class="form-textarea"></textarea><br>
-            <div class="col-md-5"><button class="container button-contact btn btn-costum mt-3 mb-5">Bagikan</button></div>
+            <input type="email" placeholder="Email Kamu" class="form-input" name="email"><br>
+            <textarea id="" placeholder="bagikan pendapatmu" class="form-textarea" name="isi_kontak"></textarea><br>
+            <div class="col-md-5"><button class="container button-contact btn btn-costum mt-3 mb-5" name="submit">Bagikan</button></div>
           </form>
         </div>
       </div>
     </div>
   </section>
+<?php
+require 'config/db.php';
+
+if(isset($_POST['submit'])){
+  $nama = htmlspecialchars($_POST['nama']);
+  $email = htmlspecialchars($_POST['email']);
+  $isi_kontak = htmlspecialchars($_POST['isi_kontak']);
+  $query = mysqli_query($conn, "INSERT INTO kontak(nama, email, isi_kontak) VALUES ('$nama', '$email', '$isi_kontak')");
+
+  if($query){
+    ?>
+  <script>
+  Swal.fire({
+  title: 'Berhasil!',
+  text: 'Terimakasih, petugas kami akan segera menghubungi anda kembali!',
+   icon: 'success'
+     }).then(() => {
+       window.location.href = 'index.php#contact';
+        });;
+    </script>
+    <?php
+  }else{
+    ?>
+      <script>
+  Swal.fire({
+  title: 'Gagal!',
+  text: 'Ada kesalahan! mohon ulangi',
+   icon: 'error'
+     });
+    </script>
+    <?php
+  }
+}
+?>
+
 
   <footer>
     <?php
