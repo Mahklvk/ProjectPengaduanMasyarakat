02cf4e586781e8c314399afc2714bfe8d5a3e367
Mahklvk <nyehggg@gmail.com>
2025-05-14 10:34:23 +0700

    change search button

--- admin/listAccountMasyarakat.php
+++ admin/listAccountMasyarakat.php
@@ -21,39 +21,57 @@ if (isset($_GET['search']) && $_GET['search'] != '') {
   <title>Daftar Akun</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
   <style>
-    .icon-search {
-  /* position: relative; */
-  left: 30px;
-  top: 17.5%;
-  /* transform: translateY(-50%); */
-  background: none;
-  border: none;
-  cursor: pointer;
-  color: #6c757d;
-}
+        .search-container {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            margin-bottom: 30px;
+        }
+        
+        .search-box {
+            width: 800px;
+            position: relative;
+        }
+        
+        .search-box input {
+            padding-left: 40px;
+            border-radius: 20px;
+        }
+        
+        .search-icon {
+            position: absolute;
+            left: 15px;
+            top: 10px;
+            color: #6c757d;
+        }
+        .btn-add-akun {
+            position: fixed;
+            bottom: 5rem;
+            right: 4rem;
+            background-color: #3E6EA2;
+            border-color: #3E6EA2;
+        }
   </style>
 </head>
 <body>
   <?php include('config/navbar.php')?>
-<div class="container mt-5">
+<div class="container mt-3">
   <h1 class="mb-4">Daftar Akun</h1>
 
   <div class="row mb-4">
     <div class="col-md-8 col-sm-6 input-icons">
-      <form class="d-flex" role="search" method="get">
-      <button type="button" class="icon-search">
-        <i class="fa fa-search fa-xl"></i>
-      </button>
-      <!-- <div class="search-container">
+      <form action="" method="GET" role="search">
+        <div class="search-container">
             <div class="search-box">
-                <i class="fa fa-search search-icon"></i>
-                <input type="text" name="search" class="form-control" placeholder="Cari Laporan" id="searchInput" value="<?php if (isset($_GET['search'])) echo $_GET['search']; ?>">
+                <i class="bi bi-search search-icon"></i>
+                <input type="text" class="form-control d-inline" placeholder="Cari Laporan" id="searchInput" name="search">
+                <button class="btn btn-outline-dark rounded-pill mt-2" type="submit">Search</button>
+                <button class="btn btn-outline-danger rounded-pill mt-2">Reset</button>
             </div>
-        </div> -->
-        <input type="text" name="search" value="" class="form-control me-2 input-field" placeholder="Search data">
-        <button class="btn btn-outline-secondary" type="submit">Search</button>
-      </form>
+        </div>
+        </form>
     </div>
     <!-- <div class="col-md-4 text-end">
       <button type="button" class="btn btn-outline-secondary"><i class="fa-solid fa-circle-plus"></i> Tambah Akun</button>
@@ -73,6 +91,11 @@ if (isset($_GET['search']) && $_GET['search'] != '') {
         <th>Aksi</th>
       </tr>
     </thead>
+
+ <a href="buat_akun.php"><button class="btn btn-primary btn-add-akun" id="addAkunBtn">
+       <i class="bi bi-plus-circle"></i> Buat Akun
+  </button></a>
+
     <tbody>
       <?php 
       if (mysqli_num_rows($result) > 0) {
