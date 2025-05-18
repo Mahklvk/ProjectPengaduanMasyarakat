5261cfc708da0025a00f49dd17a34c22551c3eb7
Mahklvk <nyehggg@gmail.com>
2025-05-19 05:16:09 +0700

    fix bug

--- admin/hapus_akun_masyarakat.php
+++ admin/hapus_akun_masyarakat.php
@@ -15,8 +15,6 @@ $query = "DELETE FROM masyarakat WHERE id_masyarakat = '$id'";
 
 if (mysqli_query($conn, $query)) {
     echo json_encode(['success' => true]);
-    session_destroy();
-    exit;
 } else {
     echo json_encode(['success' => false, 'message' => 'Gagal menghapus akun: ' . mysqli_error($conn)]);
 }
