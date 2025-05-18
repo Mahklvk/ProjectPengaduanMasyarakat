14637c0a61c69a799678d8091ea38a918f4d1035
Mahklvk <nyehggg@gmail.com>
2025-05-19 05:15:26 +0700

    added more logic

--- admin/edit_akun_admin.php
+++ admin/edit_akun_admin.php
@@ -8,27 +8,32 @@ $data = json_decode(file_get_contents("php://input"), true);
 if (
     isset($data['id_petugas']) &&
     isset($data['nik']) &&
+    isset($data['email']) &&
     isset($data['nama']) &&
     isset($data['username']) &&
-    isset($data['email']) &&
     isset($data['password']) &&
     isset($data['telp'])
 ) {
     $id = $data['id_petugas'];
     $nik = preg_replace('/\D/', '', $data['nik']);
     $nama = $data['nama'];
-    $username = $data['username'];
     $email = $data['email'];
+    $username = $data['username'];
     $password = $data['password'];
     $telp = preg_replace('/\D/', '', $data['telp']);
 
-    $query = "UPDATE petugas SET nik='$nik', nama_petugas='$nama', username='$username', email='$email', password='$password', telp='$telp' WHERE id_petugas='$id'";
-    $result = mysqli_query($conn, $query);
+    if (!empty($password)) {
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        $update_query = "UPDATE petugas SET nik='$nik',nama_petugas='$nama', email='$email',username='$username', password='$hashed_password', telp='$telp' WHERE id_petugas='$id'";
+    } else {
+        // Jika password tidak diubah
+        $update_query = "UPDATE petugas SET nik='$nik',nama_petugas='$nama', email='$emails',username='$username', telp='$telp' WHERE id_petugas='$id'";
+    }
 
-    if ($result) {
+    if (mysqli_query($conn, $update_query )) {
         echo json_encode(['success' => true]);
     } else {
-        echo json_encode(['success' => false, 'message' => 'Gagal update DB']);
+        echo json_encode(['success' => false, 'message' => 'Gagal update Database']);
     }
 } else {
     echo json_encode(['success' => false, 'message' => 'Data tidak lengkap']);
