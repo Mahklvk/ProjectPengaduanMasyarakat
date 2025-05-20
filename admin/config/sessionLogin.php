d54be7e2dcb2db30f123b68cd843246e837325fa
Mahklvk <nyehggg@gmail.com>
2025-05-20 21:55:41 +0700

    added session

--- admin/config/sessionLogin.php
+++ admin/config/sessionLogin.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 
-if($_SESSION['loginAdmin']==false){
-    header('location: loginAdmin.php');
+if (!isset($_SESSION['loginAdmin']) || $_SESSION['loginAdmin'] !== true || $_SESSION['level'] !== 'admin') {
+    // Jika bukan admin, tolak
+    header('Location: loginAdmin.php');
+    exit;
 }
+?>
